61bd0c6a9aea034d8b40122b75cc248e189b42cc
DanielRosadoRodriguez <116761593+DanielRosadoRodriguez@users.noreply.github.com>
2023-02-01 19:57:05 -0600

    Update Testing.java

--- ADA1/src/Testing.java
+++ ADA1/src/Testing.java
@@ -1,6 +1,6 @@
 public class Testing {
     public static void main(String[] args) {
-        String infixPath = "files\\infix.txt";
+        String infixPath = args[0];
         DAO dao = new DAO(infixPath);
         dao.readFile();
 
